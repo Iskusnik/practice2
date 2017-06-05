fd0a97662e6225aca5df62800ca7c71bda9a5075
IskusnikXD <IskusnikXD@ISKUSNIK>
2017-06-05 22:54:43 +0500

    1. Работа с неотсортированным массивом 2. Переделан вывод 3. Девятый тест - time limit

--- practice2/Program.cs
+++ practice2/Program.cs
@@ -9,8 +9,9 @@ namespace practice2
     class Worker
     {
         public int Num; //номер рабочего
-        public int Pay; //з/п
-        public string HelpInfo;//Строка для ответа
+        public int HelpPay; //передача
+        public int Pay; //передача
+        public int HelpNum = 0; //кому
         public Worker(int pay, int num)
         {
             Num = num;
@@ -29,7 +30,18 @@ namespace practice2
                 return 0;
             }
         }
+        public class PayNum : IComparer
+        {
+            public int Compare(object obj1, object obj2)
+            {
 
+                if (((Worker)obj1).Num < ((Worker)obj2).Num)
+                    return -1;
+                if (((Worker)obj1).Num > ((Worker)obj2).Num)
+                    return 1;
+                return 0;
+            }
+        }
     }
 
     class Program
@@ -49,51 +61,53 @@ namespace practice2
             d = new Worker[n];
 
             for (int i = 0; i < n; i++)
-                d[i] = new Worker(int.Parse(input.ReadLine()), i + 1);
+                d[i] = new Worker(avr - int.Parse(input.ReadLine()), i + 1);
 
 
 
-            //2+1* создать класс рабочего от номера и зарплаты. Отсортировать по з/п, смотреть, сколько надо.
-            //6 300
-            //200
-            //200
-            //400
-            //400
-            //300
-            //300
 
 
-            Array.Sort(d, new Worker.PaySort());
-            string[] helpInfo = new string[n];
-            for (int i = 0; i < n - 1; i++)
+            for (int i = 0; i < n; i++)
             {
-               
-                if (d[i].Pay > avr)
-                {
+
+                if (d[i].Pay > 0)
                     for (int j = 0; j < n; j++)
-                        if (d[j].Pay < avr)
+                        if (d[j].Pay < 0)
                         {
-                            helpInfo[d[j].Num - 1] = (d[i].Num).ToString() + " " + (avr - d[j].Pay).ToString();
-                            d[i].Pay += d[j].Pay - avr;
-                            d[j].Pay = avr;
-                            if (d[i].Pay <= avr)
-                                break;
+                            d[j].Pay += d[i].Pay;
+                            d[i].HelpPay = d[i].Pay;
+                            d[i].Pay = 0;
+                            d[i].HelpNum = d[j].Num;
+                            while (d[j].Pay > 0)
+                                for (int k = 0; k < n; k++)
+                                    if (d[k].Pay < 0)
+                                    {
+                                        d[k].Pay += d[j].Pay;
+                                        d[j].HelpPay = d[j].Pay;
+                                        d[j].Pay = 0;
+                                        d[j].HelpNum = d[k].Num;
+                                        j = k;
+                                    }
+                            break;
                         }
-                }
+                /*  if (d[i].Pay < 0)
+                      for (int j = 0; j < n; j++)
+                          if (d[j].Pay > 0)
+                          {
+                              d[i].Pay += d[j].Pay;
+                              d[j].HelpPay = d[j].Pay;
+                              d[j].Pay = 0;
+                              d[j].HelpNum = d[i].Num;
+                              break;
+                          }*/
             }
 
+            Array.Sort(d, new Worker.PayNum());
             for (int i = 0; i < n; i++)
             {
-                if (helpInfo[i] == null)
-                    helpInfo[i] = "0 0";
-                Console.WriteLine(helpInfo[i]);
-            }*/
-            for (int i = 0; i < n; i++)
-            {
-                if (helpInfo[i] == null)
-                    helpInfo[i] = "0 0";
-                output.WriteLine(helpInfo[i]);
+                output.WriteLine("{0} {1}", d[i].HelpNum, d[i].HelpPay);
             }
+
             input.Close();
             output.Close();
         }
